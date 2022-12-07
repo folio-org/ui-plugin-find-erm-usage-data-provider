dc4cdc61bac1eec75d578fd35153f1a6ded34ed2
elsenhans <elsenhans@users.noreply.github.com>
2022-12-07 13:27:14 +0100

    FAT-90-replace-tests-with-rtl-jest

---
+++ UDPSearch/UDPSearchContainer.test.js
@@ -0,0 +1,36 @@
+import { render, act } from '@testing-library/react';
+
+import UDPSearchContainer from './UDPSearchContainer';
+
+jest.mock('./UDPsView', () => {
+  return () => <span>UDPsView</span>;
+});
+
+const onSelectRow = jest.fn();
+
+const renderUDPSearchContainer = (mutator) => (render(
+  <UDPSearchContainer
+    mutator={mutator}
+    onSelectRow={onSelectRow}
+  />,
+));
+
+describe('UDPSearchContainer component', () => {
+  let mutator;
+
+  beforeEach(() => {
+    mutator = { query: { update: jest.fn() } };
+  });
+
+  it('should not update query when plugin is open', async () => {
+    await act(async () => {
+      renderUDPSearchContainer(mutator);
+    });
+
+    expect(mutator.query.update).not.toHaveBeenCalled();
+  });
+
+  test('should handle onSelectRow', () => {
+    expect(onSelectRow).not.toHaveBeenCalled();
+  });
+});
