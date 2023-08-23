4a58db89fb529afe399bcaac66141c2236383a2c
alb3rtino <alb3rtino@users.noreply.github.com>
2023-08-23 17:56:55 +0200

    Use @folio/jest-config-stripes

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,10 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 const path = require('path');
-
-const esModules = ['@folio', 'ky'].join('|');
+const config = require('@folio/jest-config-stripes');
 
 module.exports = {
-  collectCoverageFrom: [
-    '**/(lib|UDPSearch)/**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/test/**',
+  ...config,
+  setupFiles: [
+    ...config.setupFiles,
+    path.join(__dirname, './test/jest/setupFiles.js'),
   ],
-  coverageDirectory: './artifacts/coverage-jest/',
-  coverageReporters: ['lcov'],
-  reporters: ['jest-junit', 'default'],
-  transform: { '^.+\\.(js|jsx)$': path.join(__dirname, './test/jest/jest-transformer.js') },
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
-  moduleNameMapper: {
-    '^.+\\.(css)$': 'identity-obj-proxy',
-    '^.+\\.(svg)$': 'identity-obj-proxy',
-  },
-  testEnvironment: 'jsdom',
-  testMatch: ['**/(lib|UDPSearch)/**/?(*.)test.{js,jsx}'],
-  testPathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: [path.join(__dirname, './test/jest/jest.setup.js')],
 };
