fbff38ee16b2ec9ff714ce28c0de035522e912fd
elsenhans <elsenhans@users.noreply.github.com>
2024-07-10 15:52:47 +0200

    UIPFUDP-35-change-class-to-function-remove-defaultProps UDPSearchModal

--- src/UDPSearchModal.js
+++ src/UDPSearchModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Modal } from '@folio/stripes/components';
@@ -6,49 +6,51 @@ import { Modal } from '@folio/stripes/components';
 import UDPSearchContainer from './UDPSearchContainer';
 import css from './UDPSearch.css';
 
-export default class UDPSearchModal extends Component {
-  static propTypes = {
-    stripes: PropTypes.shape({
-      connect: PropTypes.func.isRequired
-    }).isRequired,
-    modalRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-    onUDPSelected: PropTypes.func.isRequired,
-    onClose: PropTypes.func.isRequired,
-    open: PropTypes.bool,
-    dataKey: PropTypes.string
+const UDPSearchModal = ({
+  modalRef,
+  onUDPSelected,
+  onClose,
+  open,
+  ...props
+}) => {
+  const backupModalRef = useRef();
+  const internalModalRef = modalRef || backupModalRef;
+
+  const selectUDP = (e, udp) => {
+    onUDPSelected(udp);
+    onClose();
   };
 
-  constructor(props) {
-    super(props);
+  return (
+    <Modal
+      contentClass={css.modalContent}
+      enforceFocus={false}
+      onClose={onClose}
+      size="large"
+      open={open}
+      ref={internalModalRef}
+      label={
+        <FormattedMessage id="ui-plugin-find-erm-usage-data-provider.modal.label" />
+      }
+      dismissible
+    >
+      <UDPSearchContainer
+        {...props}
+        onSelectRow={selectUDP}
+      />
+    </Modal>
+  );
+};
 
-    this.modalRef = props.modalRef || React.createRef();
-  }
+UDPSearchModal.propTypes = {
+  stripes: PropTypes.shape({
+    connect: PropTypes.func.isRequired
+  }).isRequired,
+  modalRef: PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  onUDPSelected: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool,
+  dataKey: PropTypes.string
+};
 
-  selectUDP = (e, udp) => {
-    this.props.onUDPSelected(udp);
-    this.props.onClose();
-  };
-
-  render() {
-    return (
-      <Modal
-        contentClass={css.modalContent}
-        enforceFocus={false}
-        onClose={this.props.onClose}
-        size="large"
-        open={this.props.open}
-        ref={this.modalRef}
-        label={
-          <FormattedMessage id="ui-plugin-find-erm-usage-data-provider.modal.label" />
-        }
-        dismissible
-      >
-        <UDPSearchContainer
-          {...this.props}
-          onSelectRow={this.selectUDP}
-
-        />
-      </Modal>
-    );
-  }
-}
+export default UDPSearchModal;
